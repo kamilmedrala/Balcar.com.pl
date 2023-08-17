ca0ff83539321b27a6e197d61d1364b1da40d8a6
Kamil <k.medrala99@gmail.com>
2023-08-17 22:31:24 +0200

    JobTile gradient fix

--- components/Common/JobTile.vue
+++ components/Common/JobTile.vue
@@ -3,7 +3,7 @@
         <div class="flex flex-col justify-end relative z-20 h-[100px] md:h-[200px]">
             <h3 v-if="data.title" class="text-lg md:text-2xl mb-5 font-semibold text-gray-dark">{{ data.title.rendered }}</h3>
         </div>
-        <div v-if="data.acf?.banner?.banner_image?.url" class="absolute inset-0 bottom-5">
+        <div v-if="data.acf?.banner?.banner_image?.url" class="absolute z-0 inset-0 bottom-5">
             <nuxt-picture 
             width="450"
             height="300"
@@ -13,7 +13,7 @@
             :imgAttrs="{class: 'w-full h-full object-cover'}"
             />
         </div>
-        <div class="absolute top-0 bottom-5 inset-x-0 z-10 pointer-events-none bg-gradient-to-t from-gray-lightest  to-transparent"></div>
+        <div class="absolute top-0 bottom-5 inset-x-0 z-10 pointer-events-none bg-gradient-to-t from-gray-lightest "></div>
     </CommonLink>
 </template>
 <script>
