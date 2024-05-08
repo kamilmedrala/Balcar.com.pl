3ed470ccf94cf38b5a45a370149d6fcd65288185
Kamil <k.medrala99@gmail.com>
2024-05-09 00:25:53 +0200

    pdf as popup

--- components/Section/Projects/Tile.vue
+++ components/Section/Projects/Tile.vue
@@ -1,6 +1,6 @@
 <template>
-    <li class="group border-gray-light hover:border-gold-light border border-solid transition duration-200">
-        <a :href="data.acf?.pdf_file ?? ''" target="_blank" class="flex flex-col h-full">
+    <li class="border-gray-light hover:border-gold-light border border-solid transition duration-200" @click="openPdfPopup()">
+        <div class="group flex flex-col h-full cursor-pointer">
             <div class="w-full bg-gray-lightest aspect-[55/30]">
                 <nuxt-picture
                 v-if="data.acf?.image?.url"
@@ -21,7 +21,14 @@
                     <path d="M212.00781,96a3.98105,3.98105,0,0,0-1.21924-2.8678L154.83008,37.1734a4.018,4.018,0,0,0-.606-.49683c-.05322-.03582-.11377-.05859-.16894-.09173a3.96759,3.96759,0,0,0-.51758-.27655c-.07959-.03308-.165-.04846-.2461-.07636a3.94168,3.94168,0,0,0-.50048-.15216A3.99584,3.99584,0,0,0,152,36H55.99219a12.01343,12.01343,0,0,0-12,12v88a4,4,0,0,0,8,0V48a4.00427,4.00427,0,0,1,4-4H148V96a4.0002,4.0002,0,0,0,4,4h52v36a4,4,0,0,0,8,0V96.08C212.00049,96.05267,212.00781,96.0274,212.00781,96ZM156,49.65692,198.34277,92H156ZM64,164H48a4.0002,4.0002,0,0,0-4,4v48a4,4,0,0,0,8,0V204H64a20,20,0,0,0,0-40Zm0,32H52V172H64a12,12,0,0,1,0,24Zm60-32H112a4.0002,4.0002,0,0,0-4,4v48a4.0002,4.0002,0,0,0,4,4h12a28,28,0,0,0,0-56Zm0,48h-8V172h8a20,20,0,0,1,0,40Zm88-44a4.0002,4.0002,0,0,1-4,4H184v20h20a4,4,0,0,1,0,8H184v16a4,4,0,0,1-8,0V168a4.0002,4.0002,0,0,1,4-4h28A4.0002,4.0002,0,0,1,212,168Z"/>
                 </svg>
             </div>
-        </a>
+        </div>
+        <transition name="fade">
+            <UiPopup v-if="data.acf?.pdf_file && popupActive"  @close="closePdfPopup">
+                <object :data="data.acf.pdf_file" type="application/pdf" width="100%" height="100%">
+                    <a :href="data.acf.pdf_file">Pobierz PDF</a>
+                </object>
+            </UiPopup>
+        </transition>
     </li>
 </template>
 <script>
@@ -32,5 +39,30 @@ export default {
             required: true
         }
     },
+    data(){
+        return{
+            popupActive: false
+        }
+    },
+    methods:{
+        openPdfPopup(){
+            this.popupActive = true
+        },
+        closePdfPopup(){
+            this.popupActive = false
+        },
+
+
+    }
 }
 </script>
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
---
+++ components/Ui/Popup.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="fixed z-10 inset-0 bg-black/20 flex items-center justify-center overflow-auto max-h-[100dvh] px-5 md:px-14 pt-20 pb-5 md:pb-10">
+        <div class="flex flex-col relative max-h-full max-w-7xl bg-white shadow-lg w-full h-full">
+            <div class="flex justify-end">
+                <button @click.stop="emitClosePopup()" class="group p-2 md:p-3 cursor-pointer">
+                    <svg class="stroke-gray-dark group-hover:stroke-gold-light transition duration-200" width="28" height="27" viewBox="0 0 28 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <line x1="1.70711" y1="1.29289" x2="26.7071" y2="26.2929" stroke-width="2"/>
+                        <line x1="1.29289" y1="26.2929" x2="26.2929" y2="1.29289" stroke-width="2"/>
+                    </svg>
+                </button>
+            </div>
+            <div class="overflow-auto grow">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    methods:{
+        emitClosePopup(){
+            this.$emit('close')
+        }
+    }
+}
+</script>
