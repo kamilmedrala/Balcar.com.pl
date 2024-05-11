57d0773b564b06794d0bf2daeb15521d6e19df33
Kamil <k.medrala99@gmail.com>
2024-05-12 00:24:56 +0200

    tiles safari redirect

--- components/Section/Projects/Tile.vue
+++ components/Section/Projects/Tile.vue
@@ -22,17 +22,13 @@
                 </svg>
             </div>
         </div>
-        <a ref="downloadLink" v-if="data.acf?.pdf_file" :href="data.acf.pdf_file" download title="Pobierz PDF" class="hidden"></a>
         <transition name="fade">
             <UiPopup v-if="data.acf?.pdf_file && popupActive"  @close="closePdfPopup">
                 <div class="relative w-full h-full">
                     <span class=" absolute z-0 block top-[calc(50%_-_20px)] left-[calc(50%_-_20px)] w-10 h-10 rounded-full border border-b-transparent border-gold-light animate-spin">
                     </span>
-                    <!-- <iframe :src="`https://docs.google.com/viewer?url=${data.acf.pdf_file}&embedded=true`" width="100%" height="100%" frameborder="0" class="relative z-10"></iframe> -->
                     <iframe :src="`${data.acf.pdf_file}`" width="100%" height="100%" frameborder="0" class="relative z-10"></iframe>
                 </div>
-                <!-- <object :data="data.acf.pdf_file" width="100%" height="100%">
-                </object> -->
             </UiPopup>
         </transition>
     </li>
@@ -56,8 +52,8 @@ export default {
             this.popupActive = true
         },
         handlePDFView(){
-            if ( this.getBrowserName() == 'Safari') {
-                this.$refs['downloadLink']?.click()
+            if ( this.getBrowserName() == 'Safari' && this.$props.data.acf?.pdf_file) {
+                window.location.href = this.$props.data.acf.pdf_file
             }else{
                 this.openPdfPopup()
             }
